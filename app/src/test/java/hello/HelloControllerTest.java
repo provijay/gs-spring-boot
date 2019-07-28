7e7bfd75c8dcb9ab05370c953c378095fb2e6796
Vijay Kumar <pro.vj@hotmail.com>
2019-07-28 19:44:38 +0200

    test commit

--- app/src/test/java/hello/HelloControllerTest.java
+++ app/src/test/java/hello/HelloControllerTest.java
@@ -13,7 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
+// test
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
